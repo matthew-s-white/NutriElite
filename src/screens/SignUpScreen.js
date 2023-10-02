275ef3607d5f6d02f0bc5ab86b1cb68c327aa17c
matthew-s-white <white753@purdue.edu>
2023-10-01 21:51:40 -0400

    added toasts that pop up for invalid fields

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,33 +1,83 @@
 import * as React from 'react';
 import {Theme, Button, Form, YStack, SizableText} from 'tamagui';
-import {TextInput} from 'react-native';
+import {TextInput, ToastAndroid} from 'react-native';
 
 
 const SignUpScreen = ({ navigation }) => {
+
+    const [email, onChangeEmail] = React.useState('');
+    const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+    const emailErrorMsg = "Please enter a valid email address."
+
+    const [username, onChangeUsername] = React.useState('');
+    const usernameRegex = /^[a-zA-Z0-9]{5,15}$/;
+    const usernameErrorMsg = "Username must be 5-15 alphanumeric characters."
+
+    const [password, onChangePassword] = React.useState('');
+    const passwordErrorMsg = "Password must be 8-25 characters."
+
+    const [weight, onChangeWeight] = React.useState('');
+    const weightRegex = /^(?:0|[1-9]\d+|)?(?:.?\d{0,1})?$/;
+    const weightErrorMsg = "Weight must be a number rounded to one decimal place."
+
+    const showToast = (msg) => {
+        ToastAndroid.show(`Error: ${msg}`, ToastAndroid.LONG);
+    };
+
+    const submitInfo = () => {
+        if(email === '' || !email.match(emailRegex)){
+            console.log("email is invalid");
+            showToast(emailErrorMsg);
+            return;
+        }
+        if(username === '' || !username.match(usernameRegex)){
+            console.log("username is invalid");
+            showToast(usernameErrorMsg);
+            return;
+        }
+        if(password.length < 8 || password.length > 25){
+            console.log("password is invalid");
+            showToast(passwordErrorMsg);
+            return;
+        }
+        if(weight === '' || !weight.match(weightRegex)){
+            console.log("weight is invalid");
+            showToast(weightErrorMsg);
+            return;
+        }
+        //add in call to backend
+        navigation.navigate('Home');
+    }
+
     return (
         <YStack backgroundColor="#CEFF8F" fullscreen>
-        <Form backgroundColor="#A7D36F" marginTop={100} marginBottom={100} marginLeft={30} marginRight={30} padding={30} borderRadius={20}>
+        <Form backgroundColor="#A7D36F" marginTop={20} marginLeft={30} marginRight={30} padding={30} borderRadius={20}>
             {
                 <YStack space marginBottom={50} alignSelf="center">
                     <SizableText size="$5" color="#123911">Email</SizableText>
-                    <TextInput backgroundColor="#FFFFFF" color="#000000" borderRadius={10} height={40} width={200}></TextInput>   
+                    <TextInput backgroundColor="#FFFFFF" color="#000000" borderRadius={10} height={40} width={200} value={email} onChangeText={onChangeEmail}></TextInput>   
 
                     <SizableText size="$5" color="#123911">Username</SizableText>
-                    <TextInput backgroundColor="#FFFFFF" color="#000000" borderRadius={10} height={40} width={200}></TextInput> 
+                    <TextInput backgroundColor="#FFFFFF" color="#000000" borderRadius={10} height={40} width={200} value={username} onChangeText={onChangeUsername}></TextInput> 
 
                     <SizableText size="$5" color="#123911">Password</SizableText>
-                    <TextInput backgroundColor="#FFFFFF" color="#000000" borderRadius={10} height={40} width={200}></TextInput>
+                    <TextInput backgroundColor="#FFFFFF" color="#000000" borderRadius={10} height={40} width={200} value={password} onChangeText={onChangePassword}></TextInput>
 
                     <SizableText size="$5" color="#123911">Current Weight</SizableText>
-                    <TextInput backgroundColor="#FFFFFF" color="#000000" borderRadius={10} height={40} width={200}></TextInput>    
+                    <TextInput backgroundColor="#FFFFFF" color="#000000" borderRadius={10} height={40} width={200} value={weight} onChangeText={onChangeWeight} keyboardType="numeric"></TextInput>    
                 </YStack>}
             <Form.Trigger asChild>
                 <Theme name = "dark_green">
-                    <Button onPress={() => navigation.navigate('SignUp')} fontSize={20}>Sign Up</Button>
+                    <Button onPress={submitInfo} fontSize={20}>Sign Up</Button>
                 </Theme>
             </Form.Trigger>
         </Form>
+
+
+
+
         </YStack>
+        
     );
 }
 
